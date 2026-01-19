bb8e25d1120b24232db357e9269193cc4724abf3
suvijohanna <suvi.mynttinen@metropolia.fi>
2026-01-19 11:28:05 +0200

    Add temporary dummy likes state example to MediaRow

--- src/components/MediaRow.tsx
+++ src/components/MediaRow.tsx
@@ -1,7 +1,10 @@
 import type {MediaItem} from 'hybrid-types/DBTypes';
+import {useState} from 'react';
 
 const MediaRow = (props: {item: MediaItem}) => {
   const {item} = props;
+  const [dummyLikes, setDummyLikes] = useState(0);
+
   return (
     <tr>
       <td>
@@ -12,6 +15,17 @@ const MediaRow = (props: {item: MediaItem}) => {
       <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
       <td>{item.filesize}</td>
       <td>{item.media_type}</td>
+      <td>
+        Likes {dummyLikes}
+        <button
+          onClick={() => {
+            console.log('add like to', item.title);
+            setDummyLikes(dummyLikes + 1);
+          }}
+        >
+          Add like
+        </button>
+      </td>
     </tr>
   );
 };
