c06d62ee8ad9c6340cad6d5696518a7180f82749
suvijohanna <suvi.mynttinen@metropolia.fi>
2026-01-19 11:03:21 +0200

    First component ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <>
-      <h1>My empty React app</h1>
-      <p>Hello</p>
+      <h1>My media sharing React app</h1>
+      <Home />
     </>
   );
 };
---
+++ src/components/MediaRow.tsx
@@ -0,0 +1,19 @@
+import type {MediaItem} from 'hybrid-types/DBTypes';
+
+const MediaRow = (props: {item: MediaItem}) => {
+  const {item} = props;
+  return (
+    <tr>
+      <td>
+        <img src={item.thumbnail} alt={item.title} />
+      </td>
+      <td>{item.title}</td>
+      <td>{item.description}</td>
+      <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+      <td>{item.filesize}</td>
+      <td>{item.media_type}</td>
+    </tr>
+  );
+};
+
+export default MediaRow;
